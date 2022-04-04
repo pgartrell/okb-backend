434d166b29d6b3093f3dc39bd117258d75126e2b
Sheila Strahan <smstrahan@gmail.com>
2022-04-03 20:04:21 -0600

    added therapist bio component

---
+++ Desktop/OKB Code/okb-backend2/my-project/src/api/therapist-biography/controllers/therapist-biography.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  therapist-biography controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::therapist-biography.therapist-biography');
---
+++ Desktop/OKB Code/okb-backend2/my-project/src/api/therapist-biography/routes/therapist-biography.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * therapist-biography router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::therapist-biography.therapist-biography');
